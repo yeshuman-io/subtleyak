a02adf0738cc92a985dda58c053d015632d7060d
daryl antony <daryl@thunkin.ai>
2025-02-05 16:31:39 +1100

    wip

--- src/api/admin/vehicles/models/validators.ts
+++ src/api/admin/vehicles/models/validators.ts
@@ -8,7 +8,4 @@ export const PostAdminCreateVehicleModel = z.object({
 export const PatchAdminUpdateVehicleModel = z.object({
   name: z.string().min(1).optional(),
   make_id: z.string().min(1).optional(),
-}).strict().refine(data => 
-  Object.keys(data).length > 0, 
-  { message: "At least one field must be provided for update" }
-);
+}).strict();
